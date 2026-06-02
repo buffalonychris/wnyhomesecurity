ae8a1f901b724f4a624d13dd072504d5f9fcbd25
BuffaloNYChris <buffalonychris@gmail.com>
2026-06-02 16:11:14 -0400

    Clean up homepage final CTA panel

--- src/lib/siteVersion.ts
+++ src/lib/siteVersion.ts
@@ -1,4 +1,4 @@
 // Increment patch version by +1 for each Codex PR that should be visibly confirmed after deploy.
-export const SITE_VERSION = 'v1.0.100';
+export const SITE_VERSION = 'v1.0.101';
 export const SITE_BUILD_LABEL = `WNYHS ${SITE_VERSION}`;
 export const SITE_BUILD_DATE = '2026-06-02';
