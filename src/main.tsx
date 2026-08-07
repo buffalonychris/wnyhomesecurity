e39528a8336e112388a0c613abd7bedb9c2353fd
BuffaloNYChris <60937504+buffalonychris@users.noreply.github.com>
2026-08-06 23:44:38 -0400

    feat(kaos): register isolated kaos namespace

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import KaosCommandCenter from './kaos/KaosCommandCenter';
 import RouteTransitionManager from './components/RouteTransitionManager';
 import './index.css';
 import './styles/wnyhsVisualGovernance.css';
+
+const isKaosRoute = window.location.pathname === '/kaos' || window.location.pathname.startsWith('/kaos/');
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <RouteTransitionManager />
-      <App />
+      {isKaosRoute ? <KaosCommandCenter /> : <App />}
     </BrowserRouter>
   </React.StrictMode>
 );
